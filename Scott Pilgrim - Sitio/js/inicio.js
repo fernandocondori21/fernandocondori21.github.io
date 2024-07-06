1e2adfe83f65804266b7f4b857067e79ca273a93
fernandocondori21 <131284759+fernandocondori21@users.noreply.github.com>
2024-07-05 21:57:23 -0300

    Delete Scott Pilgrim - Sitio directory

--- Scott Pilgrim - Sitio/js/inicio.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", () => {
-    document.querySelector(".menu-btn").addEventListener("click", () => {
-        document.querySelector(".nav-menu").classList.toggle("show");
-    });
-});
